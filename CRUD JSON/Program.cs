4b7bd81d4ddaf82aab9bccde0414c293e8ba660f
karremanx <xavier.karreman@gmail.com>
2019-02-24 14:26:34 +0100

    Basis JSON Object

--- CRUD JSON/Program.cs
+++ CRUD JSON/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,89 @@ namespace CRUD_JSON
     {
         static void Main(string[] args)
         {
+            Leerlingen nieuweLijst = new Leerlingen
+            {
+                LeerlingLijst = new List<Leerling>
+                {
+                    new Leerling
+                    {
+                        Naam = "Janssens",
+                        Voornaam = "Jan",
+                        Geboortedatum = new DateTime(2001, 2, 23),
+                        Klas = "5IB",
+                        Punten = new List<Punt>
+                        {
+                            new Punt("Frans", 9.5),
+                            new Punt("Wiskunde", 5.5)
+                        }
+                    },
+
+                    new Leerling
+                    {
+                        Naam = "Willems",
+                        Voornaam = "Wim",
+                        Geboortedatum = new DateTime(2001, 2, 23),
+                        Klas = "6IB",
+                        Punten = new List<Punt>
+                        {
+                            new Punt("Frans", 10),
+                            new Punt("Wiskunde", 8.5)
+                        }
+                    }
+                }
+            };
+
+            //Maak een JSON object van het C# object nieuwelijst -> SerializeObject()
+            string json = JsonConvert.SerializeObject(nieuweLijst, Formatting.Indented);
+            Console.WriteLine(json);
+            Console.ReadLine();
+
+            //Maak een C# object bestaandelijst van een JSON object -> DeserializeObject()
+            Leerlingen bestaandelijst = JsonConvert.DeserializeObject<Leerlingen>(json);
+            bestaandelijst.LeerlingLijst[0].Klas = "6IB";
+
+            //Maak terug een JSON object
+            json = JsonConvert.SerializeObject(bestaandelijst, Formatting.Indented);
+            Console.WriteLine(json);
+            Console.ReadLine();
+
+
+            foreach (Leerling ll in bestaandelijst.LeerlingLijst)
+            {
+                Console.WriteLine(ll.Naam + " " + ll.Voornaam);
+            }
+            Console.ReadLine();
         }
+
     }
+
+    public class Leerlingen
+    {
+        public List<Leerling> LeerlingLijst { get; set; }
+    }
+
+    public class Leerling
+    {
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+        public string Klas { get; set; }
+        public List<Punt> Punten { get; set; }
+        
+    }
+
+    public class Punt
+    {
+        public string Vak { get; set; }
+        public double Punten { get; set; }
+
+        public Punt(string vak, double punten)
+        {
+            this.Vak = vak;
+            this.Punten = punten;
+        }
+        
+    }
+
+
 }
